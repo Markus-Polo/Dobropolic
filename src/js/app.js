3d197b6c8605379720abcb66fc9221ed91c0bc73
Mark Polosukhin <markipulooo@gmail.com>
2023-05-22 22:40:02 +0300

    add-hero

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from './modules/functions.js'
+import * as modal from './modules/modal.js'
 
 flsFunctions.isWebp()
+modal.initModal()
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
